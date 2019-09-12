96ac64cd1064fb26dd9b3a0522dc99cda72a2057
matthew gould <mdg2838@rit.edu>
2019-09-11 23:49:24 -0700

    user interface code is done perliminarily and all program functionality has been implemented.  Code compiles, but has not been flashed onto a board.

---
+++ Project1_Timing/Objects/uart.d
@@ -0,0 +1,10 @@
+.\objects\uart.o: UART.c
+.\objects\uart.o: UART.h
+.\objects\uart.o: stm32l476xx.h
+.\objects\uart.o: C:\Users\matthew gould\AppData\Local\Arm\Packs\ARM\CMSIS\5.5.1\CMSIS\Core\Include\core_cm4.h
+.\objects\uart.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\uart.o: C:\Users\matthew gould\AppData\Local\Arm\Packs\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_version.h
+.\objects\uart.o: C:\Users\matthew gould\AppData\Local\Arm\Packs\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\uart.o: C:\Users\matthew gould\AppData\Local\Arm\Packs\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\uart.o: C:\Users\matthew gould\AppData\Local\Arm\Packs\ARM\CMSIS\5.5.1\CMSIS\Core\Include\mpu_armv7.h
+.\objects\uart.o: C:\Users\matthew gould\AppData\Local\Arm\Packs\Keil\STM32L4xx_DFP\2.2.0\Drivers\CMSIS\Device\ST\STM32L4xx\Include\system_stm32l4xx.h
--- Project1_Timing/main.c
+++ Project1_Timing/main.c
@@ -2,45 +2,110 @@
 #include "SysClock.h"
 #include "LED.h"
 #include "UART.h"
-
+#include "timer.h"
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
+
 char RxComByte = 0;
 uint8_t buffer[BufferSize];
-char str[] = "Give Red LED control input (Y = On, N = off):\r\n";
 
 int main(void){
 	//char rxByte;
-	int		a ;
-	int		n ;
-	int		i ;
-	float b;
-	
+	int n;
+	char answer;
+	int old_count;
+	int new_count;
+	int floor=950;
+	char new_range[5];
+	int measurement_count=0;
+	int measurements[1000];
+	int bins[101];
+	uint8_t temp;
+
 	System_Clock_Init(); // Switch System Clock = 80 MHz
 	LED_Init();
 	UART2_Init();
-		
-	while (1){
-		n = sprintf((char *)buffer, "a = %d\t", a);
-		n += sprintf((char *)buffer + n, "b = %f\r\n", b);
-		USART_Write(USART2, buffer, n);		
-		a = a + 1;
-		b = (float)a/100;
-		// now spin for a while to slow it down
-		for (i = 0; i < 4000000; i++)
-			;
-		
-//		USART_Write(USART2, (uint8_t *)str, strlen(str));	
-//		rxByte = USART_Read(USART2);
-//		if (rxByte == 'N' || rxByte == 'n'){
-//			Red_LED_Off();
-//			USART_Write(USART2, (uint8_t *)"LED is Off\r\n\r\n", 16);
-//		}
-//		else if (rxByte == 'Y' || rxByte == 'y'){
-//			Red_LED_On();
-//			USART_Write(USART2, (uint8_t *)"LED is on\r\n\r\n", 15);
-//		}
+	TIM_Init(TIM2);
+	n=sprintf((char *)buffer,"Lower limit is %d.\nUpper limit is %d\n. \
+	Do you wish to change these values (y/n)?",floor,floor+100);
+	USART_Write(USART2,buffer,n);
+	answer=USART_Read(USART2);
+	//allow for rerun with same limits or changing limits
+	while(1){
+	//get new limits
+		if(answer=='y'){
+				n=sprintf((char *)buffer,"\nEnter new value for lower limit in the range 50-9950: ");
+				USART_Write(USART2,buffer,n);
+			for(int i=0;i<5;i++){
+				temp=USART_Read(USART2);
+				//if non-numeric, non enter character given, restart input process
+				if((temp<0x30 || temp>0x39)&&(temp!=0xd)){  
+					n=sprintf((char *)buffer,"Invalid input\n");
+					USART_Write(USART2,buffer,n);
+					i=0;
+				}
+				//convert string of ASCII characters into an integer that represents the new lower limit
+				else if (temp=='\n'){
+					floor=atoi(new_range);
+					break;
+				}
+				else{
+					new_range[i]=temp;
+				}
+			}
+		}
+		temp=0;
+		//display prompt and then wait until enter is pushed
+		n=sprintf((char *)buffer,"Press Enter to begin\n");
+		USART_Write(USART2,buffer,n);
+		while(temp!='\n'){
+			temp=USART_Read(USART2);
+		}
+		measurement_count=0;
+		old_count=0;
+		//begin measurements
+		while (measurement_count<1000){
+				if((TIM2->SR&1)==1){
+					//first value will almost certainly be garbage so discard
+					if(measurement_count==0){;}
+					else{
+						new_count=TIM1->CCR1; //get value of counter at most recent rising edge 
+						measurements[measurement_count]=new_count-old_count; //store inter-pulse arrival time
+						old_count=new_count;
+					}
+					measurement_count++;	 
+				}
+		}
+		//sort measurements into bins
+		for(int i=0;i<1000;i++){
+			//only tally measurements that are within the limits
+			if(measurements[i]>=floor && measurements[i]<=floor+100){
+				 bins[measurements[i]-floor]++; //increment correct bin
+			}
+			measurements[i]=0; //clear array in case we want to run again 
+		}
+		for(int i=0;i<101;i++){
+			//if bin has non zero count
+			if(bins[i]>0){
+				n=sprintf((char *)buffer,"%d  %d",floor+i,bins[i]);
+				USART_Write(USART2,buffer,n);
+			}
+			bins[i]=0; //clear array in case we want to run again
+		}
+		n=sprintf((char *)buffer,"Run again(y/n)?");
+		USART_Write(USART2,buffer,n);
+		answer=USART_Read(USART2);
+		if(answer=='y'){
+			n=sprintf((char *)buffer,"\nChange limits?\n");
+			USART_Write(USART2,buffer,n);
+			answer=USART_Read(USART2); //if answer is y then if clause will handle it
+			continue;
+		}
+		else{
+			break;
+		}
 	}
 }
 
