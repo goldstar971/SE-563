fff78f2622b10d0cc91529ffce034bc43ae6616f
cadis <matthewdgould1@gmail.com>
2019-09-13 11:24:19 -0400

    fixed initalization routine for timers and now the code checks the correct flags

--- Project1_Timing/main.c
+++ Project1_Timing/main.c
@@ -15,20 +15,21 @@ int main(void){
 	//char rxByte;
 	int n;
 	char answer;
-	int old_count;
-	int new_count;
+	long long old_count;
+	long long new_count; //timer is 32-bits and max time for measurements if at most ~10 miliseconds so needs to be able to represent (2^31-1+10050). 
 	int floor=950;
 	char new_range[5];
 	int measurement_count=0;
 	int measurements[1000];
 	int bins[101];
+	char overflow_cnt=0;
 	uint8_t temp;
 
 	System_Clock_Init(); // Switch System Clock = 80 MHz
 	LED_Init();
 	UART2_Init();
 	TIM_Init(TIM2);
-	n=sprintf((char *)buffer,"Lower limit is %d.\nUpper limit is %d\n. \
+	n=sprintf((char *)buffer,"Lower limit is %d.\r\nUpper limit is %d\r\n. \
 	Do you wish to change these values (y/n)?",floor,floor+100);
 	USART_Write(USART2,buffer,n);
 	answer=USART_Read(USART2);
@@ -36,18 +37,18 @@ int main(void){
 	while(1){
 	//get new limits
 		if(answer=='y'){
-				n=sprintf((char *)buffer,"\nEnter new value for lower limit in the range 50-9950: ");
+				n=sprintf((char *)buffer,"\r\nEnter new value for lower limit in the range 50-9950: ");
 				USART_Write(USART2,buffer,n);
 			for(int i=0;i<5;i++){
 				temp=USART_Read(USART2);
 				//if non-numeric, non enter character given, restart input process
 				if((temp<0x30 || temp>0x39)&&(temp!=0xd)){  
-					n=sprintf((char *)buffer,"Invalid input\n");
+					n=sprintf((char *)buffer,"Invalid input\r\n");
 					USART_Write(USART2,buffer,n);
 					i=0;
 				}
 				//convert string of ASCII characters into an integer that represents the new lower limit
-				else if (temp=='\n'){
+				else if (temp=='\r\n'){
 					floor=atoi(new_range);
 					break;
 				}
@@ -58,25 +59,31 @@ int main(void){
 		}
 		temp=0;
 		//display prompt and then wait until enter is pushed
-		n=sprintf((char *)buffer,"Press Enter to begin\n");
+		n=sprintf((char *)buffer,"Press Enter to begin\r\n");
 		USART_Write(USART2,buffer,n);
-		while(temp!='\n'){
+		while(temp!='\r\n'){
 			temp=USART_Read(USART2);
 		}
 		measurement_count=0;
-		old_count=0;
+		overflow=0;
 		//begin measurements
+		TIM2->SR = ~TIM_SR_CC1IF //clear capture event flag
 		while (measurement_count<1000){
-				if((TIM2->SR&1)==1){
-					//first value will almost certainly be garbage so discard
-					if(measurement_count==0){;}
-					else{
-						new_count=TIM1->CCR1; //get value of counter at most recent rising edge 
-						measurements[measurement_count]=new_count-old_count; //store inter-pulse arrival time
-						old_count=new_count;
+			if(TIM2->SR&2){
+				//need to have one rising edge to occur to actually time the pulses
+				if(measurement_count==0){old_count=TIM2->CCR1;}
+				else{
+					//for the unlikely event the counter overflows mid measurement.
+					if(old_count>(2^31-1)){
+						overflow=1;//increment overflow counter
 					}
-					measurement_count++;	 
+					new_count=TIM1->CCR1; //get value of counter at most recent rising edge 
+					measurements[measurement_count]=(2^31-1)*overflow+new_count-old_count+1; //store inter-pulse arrival time
+					old_count=new_count;
 				}
+				measurement_count++;	 
+			}
+
 		}
 		//sort measurements into bins
 		for(int i=0;i<1000;i++){
@@ -98,7 +105,7 @@ int main(void){
 		USART_Write(USART2,buffer,n);
 		answer=USART_Read(USART2);
 		if(answer=='y'){
-			n=sprintf((char *)buffer,"\nChange limits?\n");
+			n=sprintf((char *)buffer,"\r\nChange limits?\r\n");
 			USART_Write(USART2,buffer,n);
 			answer=USART_Read(USART2); //if answer is y then if clause will handle it
 			continue;
--- Project1_Timing/timer.c
+++ Project1_Timing/timer.c
@@ -1,17 +1,14 @@
 #include "timer.h"
 
 void TIM_Init(TIM_TypeDef * TIMx){
-	    TIMx-> CR1&=0xf400; //disable timer and set it to count up 
-	    TIMx-> CR1|=0x0001; //disable update event  
-	    TIMx-> CR2&=0xff7f; //TIMc_CH1 pin is connected to TI1
-		TIMx->PSC |=0x4f; //set prescale clock to 1 MHZ
-		TIMx->SMCR &=0xfffffff0; //disable slave mode
-		TIMx->CCER &=0xfff3;//disable capture and compare, set as input and capture event on rising edge
-		TIMx->CCMR1 |=TIM_CCMR1_CC1S_0; //enable capture on tim5 input1
-		/*configure capture event generation*/
-		TIMx->EGR &=0xffa0;
-		TIMx->EGR |=1;
-		TIMx->ARR |=0xFFFFFFFF; //counter will count from 0 up until it reaches 2^31-1
-		TIMx->CCER |=1; //enable capture event
-		
+
+	TIMx-> CR1&=0xf400; //disable timer and set it to count up 
+	TIMx-> CR1|=0x0004; //disable update event 
+	TIMx-> CR2&=0xff7f; //only TIMc_CH1 pin is connected to TI1
+	TIMx-> PSC |=0x4f; //set prescale clock to 1 MHZ
+	TIMx->SMCR &=0xfffffff0; //disable slave mode
+	TIMx->CCER &=0xfff4;//disable capture and compare, set as input and capture event on rising edge
+	TIMx->ARR=0xFFFFFFFF; //counter will count from 0 up until it reaches 2^31-1
+	TIMx->CCER |=TIM_CCER_CCIE; //enable timer channel 1
+	TIMx->CR1|=1; //start timer
 }
