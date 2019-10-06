e497d3ac2301ebabf36dc12c9fd2b7e9a1a3b9f8
matthew gould <mdg2838@rit.edu>
2019-10-06 12:50:34 -0700

    updated timer functions

--- servo_control/timer.c
+++ servo_control/timer.c
@@ -1,15 +1,17 @@
 #include "timer.h"
-
+int pos_values[6]={800,1440,2080,2720,3360,4000};
 void TIM_Init(TIM_TypeDef * TIMx){
 	
 	RCC->APB1ENR1 |=RCC_APB1ENR1_TIM2EN; //enable timer for timer 2
 
-	TIMx-> CR1=0x4; //disable timer 2 and set it to count up 
-	TIMx->CCER &=0xfff4;//disable capture and compare, set as input and capture event on rising edge
-	TIMx->CCMR1 |= TIM_CCMR1_CC1S_0 ; //set compare input on timer ch1
-	TIMx->ARR|=0xFFFFFFFF; //counter will count from 0 up until it reaches 2^31-1
-	TIMx->PSC |=0x4f; //set prescale clock to 1 MHZ
-	TIMx->CCER |=TIM_CCER_CC1E; //enable timer channel 1
+	TIMx-> CR1=0x84; //disable timer 2 and set it to count up 
+	TIMx->CCER &=~0x11;//disable channels 1 and 2
+	TIMx-> PSC =0x27; //set prescale clock to 2mhz
+	TIMx-> ARR =0x9c3f;  // resets after 20 miliseconds
+  TIMx-> CCMR1=0x6868; // set ch1 and ch2 as outputs 
+  TIMx-> CCR1=pos_values[0]; //	 servo 1 set to move to position zero (1 % duty cycle)
+  TIMx-> CCR2=pos_values[0]; // servo 2 set to move to position zero (2% duty cycle)
+	TIMx->CCER |=TIM_CCER_CC1E+TIM_CCER_CC2E; //enable timer channel 1 and 2
 	TIMx->EGR |=0x1;//create update event, loading prescaler
 	TIMx->CR1|=TIM_CR1_CEN; //start timer 
 	
@@ -17,8 +19,22 @@ void TIM_Init(TIM_TypeDef * TIMx){
 
 void TIM_GPIO_Init(GPIO_TypeDef *GPIOx){
 	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN ; //enable clock for GPIO port A
-	GPIOx->MODER &=0xFFFFFFFC; // clear mode register for PA0
-	GPIOx->MODER|=0x2; //configure PA0 (pin23) mode as alternative function
-	GPIOx->AFR[0]&=0xFFFFFFF0; //clear alternative function register for PAO
-	GPIOx->AFR[0]|=0x1; //configure PAO with alt function 1 (CH1_tim2)	
+	GPIOx->MODER &=~0x0000000f; // clear mode register for PA1 and PA0
+	GPIOx->MODER|=0x0000000A; //configure PA0 and PA1  as alternative function
+	GPIOx->AFR[0]&=~0x0000011; //clear alternative function register for PA0 and PA1
+	GPIOx->AFR[0]|=0x11; //configure PAO and PA1 with alt function  (CHx_timx)	
+}
+
+void SysTick_Initialize(uint32_t ticks) {
+ SysTick->CTRL = 0; // Disable SysTick
+ SysTick->LOAD = ticks - 1; // Set reload register
+ // Set interrupt priority of SysTick to least urgency (i.e., largest priority value)
+ NVIC_SetPriority (SysTick_IRQn, (1<<__NVIC_PRIO_BITS) - 1);
+ SysTick->VAL = 0; // Reset the SysTick counter value
+ // Select processor clock: 1 = processor clock; 0 = external clock
+ SysTick->CTRL |= SysTick_CTRL_CLKSOURCE_Msk;
+ // Enables SysTick interrupt, 1 = Enable, 0 = Disable
+ SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk;
+ // Enable SysTick
+ SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
 }
