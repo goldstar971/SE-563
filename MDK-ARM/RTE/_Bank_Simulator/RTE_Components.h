0c8973196685ca9106d3f950e3996a6d109aec2b
Elijah Cantella <31420278+Anorwen@users.noreply.github.com>
2019-10-23 19:29:44 -0400

    changed files during wendsday
    with hacky commiting because of firewalls

---
+++ MDK-ARM/RTE/_Bank_Simulator/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Bank_Simulator' 
+ * Target:  'Bank_Simulator' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
